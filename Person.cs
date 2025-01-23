c59736387b9e8423da2402c0ee6bf8aed0a2e946
vovab <vovab@MASTURBAG>
2025-01-23 11:19:11 +0200

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Person
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+        public void Print()
+        {
+            Console.WriteLine($"Имя:{Name}\nФамилия:{Surname}\nВозраст:{Age}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Наталья";
+            person.Surname = "Шандригоз";
+            person.Age = 18;
+            person.Print();
+        }
+    }
+}
